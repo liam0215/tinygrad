23f939a2722874b6adebcb1d724d830c3e297c2a
Liam Arzola <liam0215@gmail.com>
2024-08-18 19:19:01 -0700

    cherry pick tar_extract

--- tinygrad/nn/state.py
+++ tinygrad/nn/state.py
@@ -129,6 +129,11 @@ def load_state_dict(model, state_dict:Dict[str, Tensor], strict=True, verbose=Tr
       else: v.replace(state_dict[k].to(v.device)).realize()
       if consume: del state_dict[k]
 
+def tar_extract(fn:os.PathLike) -> Dict[str, Tensor]:
+  t = Tensor(pathlib.Path(fn))
+  with tarfile.open(fn, "r") as tar:
+    return {member.name:t[member.offset_data:member.offset_data+member.size] for member in tar if member.type == tarfile.REGTYPE}
+
 # torch support!
 
 def torch_load(fn:str) -> Dict[str, Tensor]:
